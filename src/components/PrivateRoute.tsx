d6e81d1de85d95c319261a9ef9a994a7083a0a43
Felipeex <F3ZINH0HG@gmail.com>
2022-08-26 13:41:02 -0300

    ogp

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -4,7 +4,7 @@ import { useAuth } from "../contexts/AuthContext";
 type Props = { children: React.ReactElement };
 
 export const PrivateRoute: React.FC<Props> = ({ children }) => {
-  const { user, loading } = useAuth();
+  const { user } = useAuth();
 
   return user ? children : <Navigate to="/login" />;
 };
